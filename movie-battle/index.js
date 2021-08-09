6134fb8fc5fe074df6c6e26cbf4ce41fd7ec63bf
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-08-09 10:59:10 -0500

    Implement a reusable debounce function

--- movie-battle/index.js
+++ movie-battle/index.js
@@ -13,20 +13,29 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector('input')
 
-let timeoutId
-const onInput = (e) => {
-  // this will be called many times in a row
-  if (timeoutId) {
-    // first time this is called it will be undefined and skipped over
-    // 2nd time, timeoutId will be defined and timeout cleared
-    clearTimeout(timeoutId)
+//! rater limiter - apply any where we need to limit how often something should be invoked
+const debounce = (func, delay = 1000) => {
+  let timeoutId
+  return (...args) => {
+    // take all different args passed to the function
+    // this will be called many times in a row
+    if (timeoutId) {
+      // first time this is called it will be undefined and skipped over
+      // 2nd time, timeoutId will be defined and timeout cleared
+      clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => {
+      // setTimeout returns a timer ID
+      // then on further typing we set up a brand new one
+      func.apply(null, args) // we'll apply our args - take the passed in args and apply them 1 by 1 to the original function
+    }, delay) // in [delay] second call fetch data (default: 1000)
   }
-  timeoutId = setTimeout(() => {
-    // setTimeout returns a timer ID
-    // then on further typing we set up a brand new one
-    fetchData(e.target.value)
-  }, 1000) // in 1 second call fetch data
+}
+
+const onInput = (e) => {
+  fetchData(e.target.value)
 }
 
 // input event is triggered whenever there is a change in the input
-input.addEventListener('input', onInput)
+// apply rate limiter that limits invocation to once a second
+input.addEventListener('input', debounce(onInput, 500))
