fbb9798f6910e96489681be1fedb3d3c6ad9bacb
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-08-09 10:25:37 -0500

    Add input and add event listener

--- movie-battle/index.js
+++ movie-battle/index.js
@@ -1,13 +1,18 @@
-const fetchData = async () => {
+const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
       apikey: '450ca57c',
       // s: 'avengers' // for a search operation
-      i: 'tt0848228' // for a look up operation, for the exact movie
+      s: searchTerm
+      // i: 'tt0848228' // for a look up operation, for the exact movie
     }
   })
 
   console.log(response.data)
 }
 
-fetchData()
+const input = document.querySelector('input')
+// input event is triggered whenever there is a change in the input
+input.addEventListener('input', (e) => {
+  fetchData(e.target.value)
+})
