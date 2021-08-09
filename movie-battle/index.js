08c86da41d8d93c4b263a6f9e09b79ed293c2a43
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-08-09 10:08:10 -0500

    Make API connection

--- movie-battle/index.js
+++ movie-battle/index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '450ca57c',
+      // s: 'avengers' // for a search operation
+      i: 'tt0848228' // for a look up operation, for the exact movie
+    }
+  })
+
+  console.log(response.data)
+}
+
+fetchData()
