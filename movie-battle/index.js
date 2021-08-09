fad246c31ffced4e6ab62ba3cd222d741368ffa7
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-08-09 13:19:16 -0500

    Render out specific movie details

--- movie-battle/index.js
+++ movie-battle/index.js
@@ -90,5 +90,46 @@ const onMovieSelect = async (movie) => {
       i: movie.imdbID
     }
   })
-  console.log(response.data)
+  // console.log(response.data)
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+
+// render out the details of a movie
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <div class="image">
+          <img src="${movieDetail.Poster}" alt="${movieDetail.Title} Poster">
+        </div>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.BoxOffice}</p>
+      <p class="subtitle">Box Office</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification is-primary">
+      <p class="title">${movieDetail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  `
 }
